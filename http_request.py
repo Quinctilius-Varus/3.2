a7b66c6f8da48ed63a676288be8c4cc112e0c24e
Quinctilius-Varus <61425009+Quinctilius-Varus@users.noreply.github.com>
2020-02-28 13:50:42 +0300

    Create http_request.py

---
+++ http_request.py
@@ -0,0 +1,41 @@
+import requests
+import os
+
+API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+
+def translate_it(arg_text, to_lang, from_lang):
+    """
+    https://translate.yandex.net/api/v1.5/tr.json/translate ?
+    key=<API-ключ>
+     & text=<переводимый текст>
+     & lang=<направление перевода>
+     & [format=<формат текста>]
+     & [options=<опции перевода>]
+     & [callback=<имя callback-функции>]
+    :param to_lang:
+    :return:
+    """
+    params = {
+        'key': API_KEY,
+        'text': arg_text,
+        'lang': '{}-{}'.format(to_lang, from_lang),
+    }
+    response = requests.get(URL, params=params)
+    json_ = response.json()
+    return ' '.join(json_['text'])
+
+
+def translate(path_in, to_lang, from_lang='ru'):
+    with open(path_in) as file_t:
+        name = f'{path_in}'
+        translate_file = translate_it(file_t, to_lang, from_lang)
+
+        with open(os.path.join(f'd:', '\Projects', 'Netology', 'work_http_requests', 'translated', f'{path_in}'), 'w', encoding='utf8') as f:
+            print('Запись перевода в файл:', name)
+            f.write(translate_file)
+
+
+translate('DE.txt', 'de', 'ru')
+translate('ES.txt', 'es', 'en')
+translate('FR.txt', 'fr', 'de')
